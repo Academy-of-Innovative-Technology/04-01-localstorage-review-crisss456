cc5d2f588d062bce3e2837c337087b4d20618f4a
“cristopher” <curibe26@aoiths.org>
2026-01-30 13:39:52 -0500

    update

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var key = "";
 
-
 // SAVE
 document.querySelector(".save").onclick = function() {
+  
+  key = document.querySelector("#saveKey").value;
+  var value = document.querySelector("#saveValue").value;
 
+
+  localStorage.setItem(key, value);
+
+
+  document.querySelector("#saveKey").value = "";
+  document.querySelector("#saveValue").value = "";
 };
-
-
 // LOAD
 document.querySelector(".load").onclick = function() {
 
